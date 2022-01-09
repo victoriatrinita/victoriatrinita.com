9b89204ddb687d16750ec97148a9924f1b7c6151
victoriatrinita <victoriapardede@gmail.com>
2022-01-09 17:50:51 +0700

    edit layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,50 +1,23 @@
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
+	import Navigation from '$lib/Navigation.svelte';
+	import Footer from '$lib/Footer.svelte';
 	import '../styles/fonts.css';
 	import '../app.css';
 </script>
 
-<Header />
+<Navigation />
 
 <main>
 	<slot />
 </main>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+<Footer />
 
 <style>
 	:global(body > #svelte) {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
 		font-family: 'Poppins', sans-serif;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
 	}
 </style>
