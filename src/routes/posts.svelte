ebf144d1d497f40c93c94fe255e6963ac81c6603
victoriatrinita <victoriapardede@gmail.com>
2022-01-09 17:55:27 +0700

    add posts page

---
+++ src/routes/posts.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section><h1>All Posts</h1></section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+</style>
