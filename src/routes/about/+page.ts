03cd17062790489ba80005ea8d9d55474540d635
victoriatrinita <victoriapardede@gmail.com>
2022-09-29 14:47:44 +0700

    modify about page load function

--- src/routes/about/+page.ts
+++ src/routes/about/+page.ts
@@ -1,6 +1,6 @@
-export async function load({ fetch }) {
+export const load: import('./$types').PageLoad = async ({fetch}) => {
 	const articles = await fetch('/about.json').then((r) => r.json());
 	return {
 		post: articles.index
 	};
-}
+};
