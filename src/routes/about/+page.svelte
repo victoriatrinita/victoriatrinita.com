d64810f498ac9a5a536f1b0b7336bbc16a6da3ad
victoriatrinita <victoriapardede@gmail.com>
2024-06-11 23:52:55 +0900

    refactor: move about page content to svelte file

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,14 +1,71 @@
 <script lang="ts">
 	import TapedPolaroid from '$lib/components/TapedPolaroid.svelte';
 	import Article from '$lib/pages/Article.svelte';
-
-	export let data: import('./$types').PageData;
 </script>
 
 <Article>
 	<TapedPolaroid src="assets/profile/ori.jpg" alt="Self portrait" />
 
-	<section>
-		{@html data.post.content}
-	</section>
+	<h2 id="about" style:margin-top="1.5rem">About Self</h2>
+
+	<p>
+		Hello, I am Victoria Trinita Pardede, a final year Information Engineering student at
+		Universitas Gadjah Mada. I enjoy exploring the world of web development.
+	</p>
+
+	<h2 id="backstory" style:margin-top="1.5rem">This Website's Backstory</h2>
+
+	<p>
+		I remember in primary school, our teachers used to assign us to make scrapbooks that showcase
+		certain information. The topics that I still remember were 'types of traditional houses' and
+		'different kinds of animals'. I gathered information from books at the library and printed
+		pictures from the internet. I wrote down the information that I found important and glued the
+		matching pictures above. Through that, I found that I enjoy making scrapbooks.
+	</p>
+
+	<p>
+		From there, I tried making my personal scrapbook that contains information about
+		anything—something like an encyclopedia, but self-made. It was filled with article and picture
+		cutouts from magazines and newspapers. I made it with the hopes of having my own place to refer
+		to when I need information about anything. Eventually, I realize that it is the idea of
+		<i>collecting things I find useful from various places in one place</i> that I find interesting.
+	</p>
+
+	<p>
+		Hence, it became the foundation of this website creation. That is, so that I have a place to
+		keep all the things that I want to look back and refer to later on or in the future. But,
+		different from my childhood "encyclopedia", this website may not contain informational stuffs.
+		The original intention is to keep a documentation of my thoughts and the things I like and find
+		meaningful for myself that I can easily digest when I come back to it.
+	</p>
+
+	<p>
+		Other than that, in my 21st year of living, I realise that I love looking back at what I have
+		written or posted online back when I was younger. I get to know my own self, years ago. I just
+		somehow feel content when I look back at them. However, an issue that I foresee is, just like
+		how my answers and questions on Yahoo Answers went gone as the platform closed down early 2021,
+		my posts on my current social medias might also face the same fate. We never know for sure.
+		There <i>are</i> ways to download the data—and I did keep a copy of my Yahoo Answers questions and
+		answers in the form of screenshots as well as generated JSON provided by the platform before it
+		closed down—but it will never feel the same as scrolling and surfing through the platform
+		itself.
+	</p>
+
+	<p>
+		Therefore, I thought, by creating my own platform, <i>I</i> get to manage its fate. As long as I have
+		the whole code that builds up the platform (along with its backups, of course) I can always keep
+		what I have created until I actually delete it myself—which I would never think of doing. This
+		does consume a lot more effort, but hey, developing and maintaining websites and web apps is
+		what I do for a living so this counts as a media to hone my skills too.
+	</p>
+
+	<p>
+		Lastly, keeping things centralised in this platform is also an effort to minimalise having a
+		hard time trying to remember the places I have ever posted on like if they were scattered in
+		different places. Also, I need not to worry and hassle much when other platforms that I use or
+		used to use, go out of business. Not that I intend to refrain from using other platforms at all
+		though, as they do serve other purposes not coverable in this static web.
+	</p>
+
+	<p>Wrapping up, I hope this place stays as a permanent "encyclopedia" for my future self :)</p>
 </Article>
